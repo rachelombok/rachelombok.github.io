bccf6c507fe65555ad6269a39520b088b5d47bfb
Rachel Ombok <43306992+rachelombok@users.noreply.github.com>
2024-05-12 14:27:34 -0400

    hover appear bubbles

--- src/components/TierOneBubble.js
+++ src/components/TierOneBubble.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 
 export const TierOneBubble = ({ project }) => {
-    const bubbleLinkStyles = {
-    'top': project.canvasPosition[0], 
-    'left': project.canvasPosition[1]
-    }
-    return(
-        <a href='' className='bubble-link' style={bubbleLinkStyles}> 
-      <div className='tier-one-bubble' style={{ background: project.bgColor, color: project.textColor}}>
-      <img alt="project icon" src={project.icon} class="icon"/>
-        <p className='project-bubble-name'>{project.title}</p>
-        <p className='tagline'>{project.tagline}</p>
+  const bubbleLinkStyles = {
+    top: project.canvasPosition[0],
+    left: project.canvasPosition[1],
+  };
+
+  const tierOneBubbleStyles = { background: project.bgColor, color: project.textColor, };
+
+  return (
+    <a href="" className="bubble-link" style={bubbleLinkStyles}>
+      <div
+        className="tier-one-bubble"
+        style={tierOneBubbleStyles}
+      >
+        <img alt="project icon" src={project.icon} class="icon" />
+        <p className="project-bubble-name">{project.title}</p>
+          <p className="tagline">{project.tagline}</p>
       </div>
-      </a>
-    );
-}
+    </a>
+  );
+};
